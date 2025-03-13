d95813d86124066843aab759c1688f2fd1f5d019
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-13 17:09:53 -0300

    feat(dotenv): install dotenv for password uses

--- src/sendEmails.js
+++ src/sendEmails.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "login",
-    pass: "password",
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
