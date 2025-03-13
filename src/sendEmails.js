e9fa6683b2df92ef47cea6174a70f5853591577c
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-13 18:23:49 -0300

    feat(dotenv): configures dotenv

--- src/sendEmails.js
+++ src/sendEmails.js
@@ -1,22 +1,28 @@
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
+
 const nodemailer = require("nodemailer");
+const fs = require("fs");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD,
+    pass: process.env.EMAIL_ACCESS_KEY,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
-const emailList = ["email@gmail.com", "email2@gmail.com"];
+const emailList = JSON.parse(fs.readFileSync("src/emails.json", "utf8"));
 const emailHTML = "HTML CODE";
 
 async function sendEmails() {
-  for (const email of emailList) {
+  for (const { email } of emailList) {
     try {
       await transporter.sendMail({
-        from: "meu email",
+        from: `"Lucas Silva" <${process.env.EMAIL}>`,
         to: email,
         subject: "título do email",
         html: emailHTML,
