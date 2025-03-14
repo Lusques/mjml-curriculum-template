7fcdcc902d4e0bc41854ff9ecb716861889f5a39
Lucas Silva <lucascarlosjobs@gmail.com>
2025-03-14 12:21:30 -0300

    feat: text adapted to the company name

--- src/sendEmails.js
+++ src/sendEmails.js
@@ -21,17 +21,21 @@ async function sendEmails() {
   for (const { nome, email } of emailList) {
     try {
       let emailHTML = fs.readFileSync("src/output/template.html", "utf8");
-      emailHTML = emailHTML.replace(/{{empresa}}/g, nome);
+      const greetings = nome
+        ? `Desenvolvedor Front-End com experiência para agregar ao time da ${nome}!`
+        : "Desenvolvedor Front-End com experiência para agregar ao seu time!";
+      emailHTML = emailHTML.replace(/{{greetings}}/g, greetings);
       await transporter.sendMail({
         from: `"Lucas Silva" <${process.env.EMAIL}>`,
         to: email,
-        subject: `${nome}, o desenvolvedor front-end que você procura está aqui!`,
+        subject: nome
+          ? `O Desenvolvedor Front-End que a ${nome} procura está aqui!`
+          : "O Desenvolvedor Front-End que o seu time procura está aqui!",
         html: emailHTML,
         attachments: [
           {
             filename: "Curriculo_Frontend_Lucas_Silva.pdf",
             path: "src/assets/curriculo_lucas_silva.pdf",
-            
           },
         ],
       });
